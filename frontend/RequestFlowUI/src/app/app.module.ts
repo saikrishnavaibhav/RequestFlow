c3a8642b650b13235f1c0a3d8819caa65d1e1dac
saikrishnavaibhav <saikrishnavaibhav1234@gmail.com>
2023-01-19 11:08:38 +0530

    UI Initial commit

---
+++ frontend/RequestFlowUI/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { NewRequestComponent } from './new-request/new-request.component';
+import { ViewRequestComponent } from './view-request/view-request.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    NewRequestComponent,
+    ViewRequestComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/RequestFlowUI/src/app/new-request/new-request.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-request',
+  templateUrl: './new-request.component.html',
+  styleUrls: ['./new-request.component.css']
+})
+export class NewRequestComponent {
+
+}
