b8afd1af1b1837ab0e5d75dc6e158f20b33d5b22
saikrishnavaibhav <saikrishnavaibhav1234@gmail.com>
2023-01-27 21:10:45 +0530

    view request UI changes

--- frontend/RequestFlowUI/src/app/viewrequest/viewrequest.component.ts
+++ frontend/RequestFlowUI/src/app/viewrequest/viewrequest.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Request } from '../home/home.component';
+import { CSVRecord } from '../new-request/new-request.component';
 import { RequestService } from '../services/request.service';
 import { TokenStorageService } from '../services/token-storage.service';
 import { UserService } from '../services/user.service';
@@ -14,14 +15,16 @@ export class ViewrequestComponent implements OnInit {
   approved = false;
   showSuccess = false;
   showFailed = false;
-  notApproved = false;
+  rejected = false;
+  showApprove = true;
+  showReject = true;
   successMessage = "";
   failureMessage = "";
   request: any;
-
-  records:any[]=[];
+  records:CSVRecord[]=[];
   header:any[]=[];
   role:any[]=[];
+  headers:string[]=['ID','First Name', 'Last Name', 'Age', 'Salary'];
 
   constructor(private requestService: RequestService, private tokenService: TokenStorageService, private userService: UserService){}
   
@@ -35,23 +38,33 @@ export class ViewrequestComponent implements OnInit {
     this.header = this.header.map(this.toUpper);
 
     for(let req of fileData.splice(1)){
-      let data:any = req.split(",");
-      console.log(data);
+      let currentRecord = req.split(",");
+      let csvRecord:CSVRecord = {
+        id:Number.parseInt(currentRecord[0]),
+        firstName:currentRecord[1],
+        lastName:currentRecord[2],
+        age:Number.parseInt(currentRecord[3]),
+        salary:Number.parseInt(currentRecord[4])
+      }
       
-      this.records.push(data);
+      this.records.push(csvRecord);
     }
-    console.log(this.tokenService.getUser().roles[0]);
     this.isApprover = this.tokenService.getUser().roles[0] === "ROLE_APPROVER";
-    this.approved = this.request.status === "APPROVED";
+    
+    if(this.request.status === "APPROVED"){
+      this.approved = true;
+      this.successMessage = "Request approved";
+      this.showApproveRejectFalse();
+    } else if(this.request.status === "REJECTED"){
+      this.showFailed = true;
+      this.failureMessage = "Request rejected";
+      this.showApproveRejectFalse();
+    }
   }
 
   toUpper = function(header : any){ 
     return header.toUpperCase();
   };
-
-  submit(){
-
-  }
 
   approve(approve:boolean){
     this.userService.approveRequest(this.tokenService.getUser().id, this.request.id,approve).subscribe(
@@ -68,22 +81,34 @@ export class ViewrequestComponent implements OnInit {
           this.request.status = "REJECTED"
           this.request.approvals[0].status = "REJECTED";
         }
+        this.showApproveRejectFalse();
         setTimeout(() => {
           this.showSuccess=false;
         }, 2000);
       },
       error=>{
       console.error(error);
-      this.showFailed = true;
+        this.showReject = true;
       if(approve)
         this.failureMessage = "Request Approve failed";
       else
         this.failureMessage = "Request Reject failed";
-
+        this.showApproveRejectTrue();
         setTimeout(() => {
           this.showFailed=false;
         }, 2000);
       }
     );
   }
+
+  showApproveRejectFalse(){
+    this.showApprove = false;
+    this.showReject = false;
+  }
+
+  showApproveRejectTrue(){
+    this.showFailed = true;
+    this.showApprove = true;
+  }
+  
 }
